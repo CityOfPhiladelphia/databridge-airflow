0fe62247b97154036ae3bc6f95436f970538a7fb
Peter Dannemann <peter.dannemann@phila.gov>
2019-04-25 22:53:42 +0000

    Fix indentation in databridge_operators

--- plugins/databridge_operators.py
+++ plugins/databridge_operators.py
@@ -1,3 +1,4 @@
+from airflow.hooks.base_hook import BaseHook
 from airflow.contrib.operators.awsbatch_operator import AWSBatchOperator
 from airflow.utils.decorators import apply_defaults
 
@@ -16,7 +17,6 @@ class DataBridgeToS3Operator(AWSBatchOperator):
     :type table_name: string
     """
 
-    template_fields = ('table_schema', 'table_name')
     ui_color = '#ededed'
     
     @apply_defaults
@@ -25,28 +25,28 @@ class DataBridgeToS3Operator(AWSBatchOperator):
         table_schema,
         table_name,
         *args, **kwargs):
-    super(DataBridgeToS3Operator, self).__init__(
-        job_name='db_to_s3_{}_{}'.format(self.table_schema, self.table_name),
-        job_definition='test_extract_and_load_to_databridge',
-        job_queue='databridge-airflow',
-        region_name='us-east-1',
-        overrides={
-            'command': [
-                'python3 /extract_and_load_to_databridge.py', 
-                'extract', 
-                'db_type={}'.format(db_conn.conn_type), 
-                'db_host={}'.format(db_conn.host), 
-                'db_user={}'.format(db_conn.login), 
-                'db_password={}'.format(db_conn.password), 
-                'db_name={}'.format(db_conn.extra), 
-                'db_port={}'.format(db_conn.port), 
-                'db_table_schema={}'.format(self.table_schema), 
-                'db_table_name={}'.format(self.table_name), 
-                's3_bucket=citygeo-airflow-databridge2',
-            ],
-        },
-        task_id='db_to_s3_{}_{}'.format(self.table_schema, self.table_name),
-        *args, **kwargs)
+        super(DataBridgeToS3Operator, self).__init__(
+            job_name='db_to_s3_{}_{}'.format(table_schema, table_name),
+            job_definition='test_extract_and_load_to_databridge',
+            job_queue='databridge-airflow',
+            region_name='us-east-1',
+            overrides={
+                'command': [
+                    'python3 /extract_and_load_to_databridge.py', 
+                    'extract', 
+                    'db_type={}'.format(db_conn.conn_type), 
+                    'db_host={}'.format(db_conn.host), 
+                    'db_user={}'.format(db_conn.login), 
+                    'db_password={}'.format(db_conn.password), 
+                    'db_name={}'.format(db_conn.extra), 
+                    'db_port={}'.format(db_conn.port), 
+                    'db_table_schema={}'.format(table_schema), 
+                    'db_table_name={}'.format(table_name), 
+                    's3_bucket=citygeo-airflow-databridge2',
+                ],
+            },
+            task_id='db_to_s3_{}_{}'.format(table_schema, table_name),
+            *args, **kwargs)
 
 class S3ToDataBridge2Operator(AWSBatchOperator):
     """
@@ -58,7 +58,6 @@ class S3ToDataBridge2Operator(AWSBatchOperator):
     :type table_name: string
     """
 
-    template_fields = ('table_schema', 'table_name')
     ui_color = '#ededed'
     
     @apply_defaults
@@ -67,28 +66,28 @@ class S3ToDataBridge2Operator(AWSBatchOperator):
         table_schema,
         table_name,
         *args, **kwargs):
-    super(S3ToDataBridge2Operator, self).__init__(
-        job_name='s3_to_databridge2_{}_{}'.format(self.table_schema, self.table_name),
-        job_definition='test_extract_and_load_to_databridge',
-        job_queue='databridge-airflow',
-        region_name='us-east-1',
-        overrides={
-            'command': [
-                'python3 /extract_and_load_to_databridge.py',
-                'write',
-                'db_type={}'.format(db2_conn.conn_type),
-                'db_host={}'.format(db2_conn.host),
-                'db_user={}'.format(db2_conn.login),
-                'db_password={}'.format(db2_conn.password),
-                'db_name={}'.format(db2_conn.extra),
-                'db_port={}'.format(db2_conn.port),
-                'db_table_schema={}'.format(table_schema),
-                'db_table_name={}'.format(table_name),
-                's3_bucket=citygeo-airflow-databridge2',
-            ],
-        },
-        task_id='s3_to_databridge2_{}_{}'.format(self.table_schema, self.table_name),
-        *args, **kwargs)
+        super(S3ToDataBridge2Operator, self).__init__(
+            job_name='s3_to_databridge2_{}_{}'.format(table_schema, table_name),
+            job_definition='test_extract_and_load_to_databridge',
+            job_queue='databridge-airflow',
+            region_name='us-east-1',
+            overrides={
+                'command': [
+                    'python3 /extract_and_load_to_databridge.py',
+                    'write',
+                    'db_type={}'.format(db2_conn.conn_type),
+                    'db_host={}'.format(db2_conn.host),
+                    'db_user={}'.format(db2_conn.login),
+                    'db_password={}'.format(db2_conn.password),
+                    'db_name={}'.format(db2_conn.extra),
+                    'db_port={}'.format(db2_conn.port),
+                    'db_table_schema={}'.format(table_schema),
+                    'db_table_name={}'.format(table_name),
+                    's3_bucket=citygeo-airflow-databridge2',
+                ],
+            },
+            task_id='s3_to_databridge2_{}_{}'.format(table_schema, table_name),
+            *args, **kwargs)
 
 class S3ToCartoOperator(AWSBatchOperator):
     """
@@ -100,7 +99,6 @@ class S3ToCartoOperator(AWSBatchOperator):
     :type table_name: string
     """
     
-    template_fields = ('table_schema', 'table_name')
     ui_color = '#ededed'
     
     @apply_defaults
@@ -109,19 +107,19 @@ class S3ToCartoOperator(AWSBatchOperator):
         table_schema,
         table_name,
         *args, **kwargs):
-    super(S3ToCartoOperator, self).__init__(
-        job_name='s3_to_carto_{}_{}'.format(self.table_schema, self.table_name),
-        job_definition='test_extract_and_load_to_databridge',
-        job_queue='databridge-airflow',
-        region_name='us-east-1',
-        overrides={
-            'command': [
-                'python3 /extract_and_load_to_databridge.py',
-                'carto_update_table',
-                'carto_connection_string={}'.format(carto_conn.password),
-                's3_bucket=citygeo-airflow-databridge2',
-                'json_schema_file={}__{}.json'.format(self.table_schema, self.table_name),
-            ],
-        },
-        task_id='s3_to_carto_{}_{}'.format(self.table_schema, self.table_name),
-        *args, **kwargs)
+        super(S3ToCartoOperator, self).__init__(
+            job_name='s3_to_carto_{}_{}'.format(table_schema, table_name),
+            job_definition='test_extract_and_load_to_databridge',
+            job_queue='databridge-airflow',
+            region_name='us-east-1',
+            overrides={
+                'command': [
+                    'python3 /extract_and_load_to_databridge.py',
+                    'carto_update_table',
+                    'carto_connection_string={}'.format(carto_conn.password),
+                    's3_bucket=citygeo-airflow-databridge2',
+                    'json_schema_file={}__{}.json'.format(table_schema, table_name),
+                ],
+            },
+            task_id='s3_to_carto_{}_{}'.format(table_schema, table_name),
+            *args, **kwargs)
