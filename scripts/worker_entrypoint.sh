a55510a3114aaff74a157a9bc7b306e0e5d11668
Peter Dannemann <peter.dannemann@phila.gov>
2019-05-01 18:00:51 +0000

    Add comment on how exec works in entrypoint scripts

--- scripts/worker_entrypoint.sh
+++ scripts/worker_entrypoint.sh
@@ -3,4 +3,5 @@
 # Resolve hostname to fix OID generation error by cx_Oracle
 curl -s http://169.254.169.254/latest/meta-data/local-hostname | cut -d. -f1 | awk '{print $1" localhost"}' > /tmp/HOSTALIASES
 
+# This is needed to use both an entrypoint and a command with Docker
 exec "${@}"
