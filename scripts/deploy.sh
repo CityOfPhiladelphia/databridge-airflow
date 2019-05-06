62bafd98222afd5c0086c6a1363dfbed5a086cd5
Peter Dannemann <peter.dannemann@phila.gov>
2019-05-06 16:41:37 -0400

    Download the backup of the database when deploying

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,5 +5,8 @@ set -e
 # Enter the application's directory
 cd /home/ubuntu/databridge-airflow
 
+# Copy the latest database backup
+aws s3 cp s3://citygeo-airflow-databridge2/pgdata pgdata --recursive
+
 # Rebuild and restart the server
 sudo docker-compose -f docker-compose.yml up -d --build
