c980e3cd2de3ba3307895bf185587fd41d0a0acb
Peter Dannemann <peter.dannemann@phila.gov>
2019-05-07 10:48:47 -0400

    Switch back to backing up and restoring database
    CodeDeploy won't work with persisting pgdata

--- scripts/backup_db.sh
+++ scripts/backup_db.sh
@@ -7,6 +7,6 @@ cd /home/ubuntu/databridge-airflow
 
 # Dump to s3
 if [ -d "/home/ubuntu/databridge-airflow/pgdata" ]; then
-    mkdir -p /home/ubuntu/databridge-airflow/pgdata
     aws s3 cp /home/ubuntu/databridge-airflow/pgdata/ s3://citygeo-airflow-databridge2/pgdata --recursive
+    sudo rm -r /home/ubuntu/databridge-airflow/pgdata/
 fi
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,5 +5,9 @@ set -e
 # Enter the application's directory
 cd /home/ubuntu/databridge-airflow
 
+mkdir pgdata
+# Copy the latest database backup
+aws s3 cp s3://citygeo-airflow-databridge2/pgdata pgdata --recursive
+
 # Rebuild and restart the server
 sudo docker-compose -f docker-compose.yml up -d --build
