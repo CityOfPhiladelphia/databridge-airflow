ee3bf242a96f3cfe4913ea81bf3448800a059db1
Peter Dannemann <peter.dannemann@phila.gov>
2019-05-17 12:10:09 -0400

    Fix retry logic

--- dags/knack_factory.py
+++ dags/knack_factory.py
@@ -27,7 +27,7 @@ def knack_dag_factory(
         'owner': 'airflow',
         'start_date': datetime(2019, 5, 10, 0, 0, 0) - timedelta(hours=8),
         'on_failure_callback': SlackNotificationOperator.failed,
-        'retries': 2 if 'PROD' in os.environ else 0,
+        'retries': 2 if os.environ['ENVIRONMENT'] == 'PROD' else 0,
         'retry_delay': timedelta(minutes=5)
     }
 
